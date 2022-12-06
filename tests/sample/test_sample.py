b2b0b67e2c74fa4284f4a689cf350c8614bfc24e
Gleams API user <Stephen.Swannell+ghapi@gmail.com>
2022-12-06 21:55:28 +0000

    force testt failure

--- tests/sample/test_sample.py
+++ tests/sample/test_sample.py
@@ -11,30 +11,23 @@ logger.add(sys.stderr, level="DEBUG")
 @pytest.mark.colour("Red")
 def test_sample_1():
     logger.debug("Executing test: test_sample_1")
-    logger.debug("Delaying test: test_sample_1")
-    time.sleep(4)
     logger.debug("Finished test: test_sample_1")
 
 
 @pytest.mark.colour("Red", "Yellow")
 def test_sample_2():
     logger.debug("Executing test: test_sample_2")
-    logger.debug("Delaying test: test_sample_2")
-    time.sleep(6)
+    assert False
     logger.debug("Finished test: test_sample_2")
 
 
 @pytest.mark.colour("Blue")
 def test_sample_3():
     logger.debug("Executing test: test_sample_3")
-    logger.debug("Delaying test: test_sample_3")
-    time.sleep(2)
     logger.debug("Finished test: test_sample_3")
 
 
 @pytest.mark.colour("Green", "Yellow")
 def test_sample_4():
     logger.debug("Executing test: test_sample_4")
-    logger.debug("Delaying test: test_sample_4")
-    time.sleep(10)
     logger.debug("Finished test: test_sample_4")
